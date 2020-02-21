1d77c9a7914095632d482552631a40709504ec35
sadig2 <clanzu2@yahoo.com>
2020-02-21 17:59:35 +0400

    first

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,21 @@
+import classes from "./Burger.module.css";
+import React from "react";
+import BurgerIngredient from "./BurgerIgredient/BurgerIngredient";
+
+const burger = props => {
+  const transformedIngredient = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
